eb001d06faa961eb8957e6662b516216704d8af6
AbakusNantis <privat@bastianknaus.de>
2025-07-16 02:54:19 +0200

    did some things, but not sure if the fucking List Manager is gone forever

---
+++ ScrapeEntry/__init__.py
@@ -0,0 +1,43 @@
+from playwright.sync_api import sync_playwright
+from dataclasses import dataclass
+import os, re
+from utils.KVManager import KeyVaultManager
+
+class FreelanceClient:
+    def __init__(self, headless: bool = True):
+        self._pw = sync_playwright().start()
+        self.browser = self._pw.chromium.launch_persistent_context(
+            user_data_dir="profile_freelance",
+            headless=headless,
+            viewport={'width':1280, 'height':800}
+        )
+        self.page = self.browser.new_page()
+
+    def login(self) -> None:
+        self.page.goto("https://www.freelance.de/login.php")
+        self.page.fill("#username", KeyVaultManager().get_secret('freelance-username'))
+        self.page.fill("#password", KeyVaultManager().get_secret('freelance-password'))
+        self.page.click("button[type=submit]")
+        self.page.wait_for_selector("nav >> text=Mein Profil")  # Erfolgskriterium
+        print("Login success.")
+
+    def close(self):
+        self.browser.close()
+        self._pw.stop()
+
+if __name__ == "__main__":
+    FreelanceClient().login()
+
+
+@dataclass
+
+class ScrapeEntry:
+    """
+    Scrapes a freelance URL for relevant content
+    """
+    def __init__(self, url: str):
+        self.url = url
+    
+    def scrape(self):
+        return
+    
