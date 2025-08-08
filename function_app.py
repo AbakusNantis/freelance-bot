daef3648b2e4cdf13f1e3baad30c5002abbbf3ef
AbakusNantis <bastian.knaus@gmail.com>
2025-08-08 09:45:44 +0200

    feat: added poetry setup, changed function setup to blueprints

--- function_app.py
+++ function_app.py
@@ -1,23 +1,9 @@
 # function_app.py
-import json
-import logging
 
 import azure.functions as func
-from azure.functions import FunctionApp, AuthLevel
-from ScrapeNewEntries import ScrapeNewEntries
+
+from scrapeNewEntries import bp as bp_scrapeNewEntries
 
 app = func.FunctionApp()
 
-@app.route(route="scrape-new-entries", auth_level=func.AuthLevel.ANONYMOUS, methods=["GET"])
-
-def scrapeNewEntries(req: func.HttpRequest) -> func.HttpResponse:
-    try:
-        urls = ScrapeNewEntries().scrape()
-        return func.HttpResponse(
-            json.dumps(urls),
-            status_code=200,
-            mimetype="application/json"
-        )
-    except Exception as e:
-        logging.exception("Scrape fehlgeschlagen")
-        return func.HttpResponse(f"Error: {e}", status_code=500)
+app.register_functions(bp_scrapeNewEntries)
